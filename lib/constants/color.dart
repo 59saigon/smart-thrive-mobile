0123f94061525eace43c3f4b8c4c9bd6d1abc987
Nguyen Xuan Viet <imnviet@gmail.com>
2024-06-03 17:50:39 +0700

    Update footer menu & Add assets folder

---
+++ lib/constants/color.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFFF5F7F8);
+const Color kColor1 = Color(0xFFF4CE14);
+const Color kColor2 = Color(0xFF495E57);
+const Color kColor3 = Color(0xFF45474B);
--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,44 @@
 import 'package:flutter/material.dart';
-import 'pages/homepage.dart';
-import 'pages/login_page.dart';
-import 'pages/register_page.dart';
+import 'package:smart_thrive_mobile/pages/base_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
+  // root of application
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: RegisterPage(),
+      title: 'Smart Thrive',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Poppins',
+        textTheme: const TextTheme(
+          titleLarge: TextStyle(
+            fontSize: 20,
+            color: Colors.white,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyLarge: TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w500,
+          ),
+          bodyMedium: TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+          displayMedium: TextStyle(
+            fontWeight: FontWeight.w700,
+            fontSize: 18,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: const BaseScreen(),
     );
   }
 }
