47a31aac2394cf387bb48003681c0e735c699c1a
Nguyen Xuan Viet <imnviet@gmail.com>
2024-06-07 13:54:22 +0700

    Implement course data, details screen & modified course_screen, course_container

--- lib/screens/course_screen.dart
+++ lib/screens/course_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:smart_thrive_mobile/models/course.dart';
+import 'package:smart_thrive_mobile/widgets/course_container.dart';
 import 'package:smart_thrive_mobile/widgets/custom_icon_button.dart';
-
-import '../widgets/course_container.dart';
 
 class CourseScreen extends StatefulWidget {
   const CourseScreen({Key? key}) : super(key: key);
@@ -34,20 +34,34 @@ class _CourseScreenState extends State<CourseScreen> {
                         ),
                       ),
                       Positioned(
-                        left: 0,
-                        child: CustomIconButton(
-                          child: const Icon(Icons.arrow_back),
-                          height: 35,
-                          width: 35,
-                          onTap: () => Navigator.pop(context),
-                        ),
-                      ),
+                          left: 0,
+                          child: CustomIconButton(
+                            child: const Icon(Icons.arrow_back),
+                            height: 35,
+                            width: 35,
+                            onTap: () => Navigator.pop(context),
+                          )),
                     ],
                   ),
                 ),
                 const SizedBox(
                   height: 15,
                 ),
+                Expanded(
+                    child: ListView.separated(
+                  shrinkWrap: true,
+                  itemBuilder: (_, index) {
+                    return CourseContainer(
+                      course: courses[index],
+                    );
+                  },
+                  separatorBuilder: (context, _) {
+                    return const SizedBox(
+                      height: 10,
+                    );
+                  },
+                  itemCount: courses.length,
+                )),
               ],
             ),
           ),
---
+++ lib/screens/details_screen.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:smart_thrive_mobile/widgets/custom_icon_button.dart';
+
+class DetailsScreen extends StatefulWidget {
+  final String title;
+  const DetailsScreen({super.key, required this.title});
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: SystemUiOverlayStyle.dark,
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IntrinsicHeight(
+                  child: Stack(
+                    children: [
+                      Align(
+                        child: Text(
+                          'Flutter',
+                          style: Theme.of(context).textTheme.displayMedium,
+                        ),
+                      ),
+                      Positioned(
+                        left: 0,
+                        child: CustomIconButton(
+                          child: const Icon(Icons.arrow_back),
+                          height: 35,
+                          width: 35,
+                          onTap: () => Navigator.pop(context),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
